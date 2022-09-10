2a8260995c1bbe4cc92f8cbfef1e2fdd6ed992cb
EC2 Default User <102804114+Natumesiki@users.noreply.github.com>
2022-09-10 16:15:12 +0000

    [4-25 ADD]課題3 Bootsteap導入・記述まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  # get 'users/edit'
+  # get 'users/show'
+  # get 'books/index'
+  # get 'books/show'
+  # get 'books/new'
+  # get 'homes/top'
   devise_for :users
+       root :to => "homes#top"
+get '/homes/about' => 'homes#about' , as: 'about'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
