5efbac97bc17d6c679515d434378734fb53fd8a8
EC2 Default User <102804114+Natumesiki@users.noreply.github.com>
2022-09-12 12:41:16 +0000

    [4-25 ADD]課題3 bookのindexの左側作成途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,31 @@
 class BooksController < ApplicationController
   def index
+    # @book = Book.all.page(params[:page])
   end
 
   def show
+    # @book = Book.find(params[:id])
+
   end
 
   def new
+     @book = Book.new
   end
-end
+
+   def create
+     @book = Book.new(book_params)
+     @book.user_id = current_user.id
+   if @book.save
+     redirect_to book_path
+   else
+     render :new
+   end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title,:body,:image)
+  end
+
+ end
+ end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,32 @@
 class UsersController < ApplicationController
+  # before_action :authenticate_user!, only: [:show]
+
   def edit
+     @user = User.find(params[:id])
+     @user = User.new
   end
 
   def show
+     @user = User.find(params[:id])
+     @book = @user.book.page(params[:page])
+  end
+
+   def create
+    @user = User.new(user_params)
+    @user.user_id = current_user.id
+    @user.save
+    redirect_to user_path
+  end
+
+  def update
+  # user = User.find(params[:id])
+  # user.update(user_params)
+  # redirect_to user_path(user.id)
+ end
+
+  private
+
+  def user_params
+     params.require(:user).permit(:name, :profile_image,:introduction)
   end
 end
