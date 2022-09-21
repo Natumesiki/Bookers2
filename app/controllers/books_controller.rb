1d10a115f7e631ade0b08915c18dac93c5ba3a45
EC2 Default User <102804114+Natumesiki@users.noreply.github.com>
2022-09-21 10:23:41 +0000

    [4-25 ADD]課題3 bookのindexの左側作成途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,31 +1,46 @@
 class BooksController < ApplicationController
+ before_action :authenticate_user!, only: [:show,:edit,:index,]
 
   def index
     @book = Book.new
-    @book = Book.all
+    @books = Book.all
+    @user = current_user
   end
 
   def show
      @book = Book.find(params[:id])
-
+     @user = current_user
+    
   end
 
+  def edit
+      @book = Book.find(params[:id])
+      @user = current_user
+  end
 
    def create
-     book = Book.new(book_params)
-     book.save
-     redirect_to '/books'
-    #  @book.user_id = current_user.id
-#   if @book.save
-#      redirect_to book_path
-#   else
-#      render :index
+     @book = Book.new(book_params)
+      @book.user_id = current_user.id
+      @book.save
+     redirect_to "/books"
+
    end
 
-  private
+ def destroy
+     @book = Book.find(params[:id])
+     @book.delete
+     redirect_to "/books/index"
+ end
+
+   def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+ private
 
   def book_params
     params.require(:book).permit(:title,:body,:image)
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   # get 'users/edit'
   # get 'users/show'
-  # get 'books/index'
+#   get  'books/index'
   # get 'books/show'
   # get 'books/new'
   # get 'homes/top'
   devise_for :users
        root :to => "homes#top"
 get 'homes/about' => 'homes#about' , as: 'about'
-   resources :books, only:[:index,:show,:edit,:update]
+
+   resources :books, only:[:index,:show,:edit,:update,:create,:destroy]
    resources :users, only:[:show,:edit,:update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # error prohibited this user from being saved:
