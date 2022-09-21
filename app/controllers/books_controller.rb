904b8bd0978b2abbcfa88679cad923b0bd2c3802
EC2 Default User <102804114+Natumesiki@users.noreply.github.com>
2022-09-21 10:50:17 +0000

    [4-25 ADD]課題3 bookのindexの左側作成途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,7 +10,7 @@ class BooksController < ApplicationController
   def show
      @book = Book.find(params[:id])
      @user = current_user
-    
+
   end
 
   def edit
@@ -29,7 +29,7 @@ class BooksController < ApplicationController
  def destroy
      @book = Book.find(params[:id])
      @book.delete
-     redirect_to "/books/index"
+     redirect_to book_path(book.id)
  end
 
    def update
