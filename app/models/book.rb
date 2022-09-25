d098f61c482323a08380569cc18df26247b7c6aa
EC2 Default User <102804114+Natumesiki@users.noreply.github.com>
2022-09-25 16:35:04 +0000

    [4-25 ADD]課題3 bookのindexの左側作成途中

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,20 +1,16 @@
 class Book < ApplicationRecord
-  has_one_attached :Profile_image
+  has_one_attached :profile_image
    belongs_to :user
   validates :title, presence: true
   validates :body, presence: true
 
-
-
-#  def get_image
-#      unless image.attached?
-#       file_path = Rails.root.join('app/assets/images/no_image.jpg')
-#       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-#     end
-#     image
-#  end
-
-#   def favorited_by?(user)
-#     favorites.exists?(user_id: user.id)
-#   end
+ def get_image
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image
   end
+
+ end
+
